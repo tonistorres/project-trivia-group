8ce29cae5e632225e2ba85d302e17afed2172e93
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-01-14 19:16:45 -0300

    Questões e respostas sendo renderizadas na tela

--- src/components/GameArea/GameArea.jsx
+++ src/components/GameArea/GameArea.jsx
@@ -8,14 +8,34 @@ class GameArea extends Component {
     // const first = questions[0];
     // const { category, question, correct_answer } = first;
     // console.log(first);
-    return (
-      <main>
-        Olha o game aí
-        <h2 data-testid="question-category">
-          {/* { category } */}
-        </h2>
-      </main>
-    );
+
+    if (questions.length > 0) {
+      return (
+        <main>
+          <h2 data-testid="question-category">
+            { questions[0].category }
+          </h2>
+          <span data-testid="question-text">
+            { questions[0].question }
+          </span>
+          <div data-testid="answer-options">
+            <button type="button" data-testid="correct-answer">
+              { questions[0].correct_answer }
+            </button>
+            <button type="button" data-testid={`wrong-answer`}>
+              { questions[0].incorrect_answers[0] }
+            </button>
+            <button type="button" data-testid={`wrong-answer`}>
+              { questions[0].incorrect_answers[1] }
+            </button>
+            <button type="button" data-testid={`wrong-answer`}>
+              { questions[0].incorrect_answers[2] }
+            </button>
+          </div>
+        </main>
+      );
+    }
+    return <h1>Carregando...</h1>;
   }
 }
 const mapStateToProps = (state) => ({
